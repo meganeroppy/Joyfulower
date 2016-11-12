d0748b7566bd1cb1378176069dad6bada325fdab
m.roppy <roppy@hotmail.co.jp>
2016-11-12 18:10:24 +0900

    パラメータを定義

---
+++ Assets/APIParameter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// API のパラメータ群
+/// </summary>
+namespace APIParameter 
+{
+	public class ResponseParameter{
+
+		/// <summary>
+		/// GPS座標データ
+		/// </summary>
+		public struct GPS
+		{
+			/// <summary>
+			/// 経度
+			/// </summary>
+			public float longitude;
+
+			/// <summary>
+			/// 緯度
+			/// </summary>
+			public float latitude;
+
+			/// <summary>
+			/// 高度
+			/// </summary>
+			public float altitude;
+		}
+
+		/// <summary>
+		/// 座標
+		/// </summary>
+		public GPS gps;
+
+		/// <summary>
+		/// 幸福度
+		/// </summary>
+		public int happiness;
+
+	}
+}
