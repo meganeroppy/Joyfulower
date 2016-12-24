e80b2a4275e1fa8a39d0350e72b6b41bea5ec247
m.roppy <roppy@hotmail.co.jp>
2016-12-24 18:33:43 +0900

    花束生成クラスを作成

---
+++ Assets/Script/BouquetMaker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 花束作成クラス
+/// </summary>
+public class BouquetMaker : MonoBehaviour {
+
+	/// <summary>
+	/// 花束ベース
+	/// </summary>
+	Transform bouquetbase;
+
+	/// <summary>
+	/// 花パーツ
+	/// </summary>
+	List<GameObject> parts;
+
+	/// <summary>
+	/// 現在掴んでいる花パーツ
+	/// </summary>
+	GameObject heldPart;
+
+	/// <summary>
+	/// 花を追加できる距離メートル
+	/// </summary>
+	public float validRange = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
