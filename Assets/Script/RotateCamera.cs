a0e6904400b0f001ef1e05ee4c7ce39d5b4ed0c1
ZBOX-HE3PEEU5I9\miken <roppy@hotmail.co.jp>
2017-01-15 21:29:15 +0900

    蝶々を追加

---
+++ Assets/Script/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class RotateCamera : MonoBehaviour
+{
+	[SerializeField]
+	Transform rotCamera;
+
+	void Update () 
+	{
+	//	transform.rotation = Quaternion.Euler (0, rotCamera.transform.rotation.y, 0);
+		transform.LookAt( rotCamera );
+	}
+
+	void Disable() 
+	{
+		this.gameObject.SetActive(false);
+	}
+}
