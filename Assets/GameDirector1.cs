94378fa6752a6cae95072b977599514d5bd0039e
m.roppy <roppy@hotmail.co.jp>
2017-01-22 01:24:26 +0900

    UIを修正（！）

--- Assets/GameDirector1.cs
+++ Assets/GameDirector1.cs
@@ -3,19 +3,22 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameDirector : MonoBehaviour 
-{
-	public static GameDirector instance;
+public class GameDirector_ : MonoBehaviour {
+
+	public static GameDirector_ instance;
 
     // (1)何を(2)どれだけ持っているか、を把握するためのList
+    //private List<string> name_List = new List<string>(); //持っている種類リスト
+    //private List<int> num_List = new List<int>(); //持っている数リスト
 
     //ItemPanel・ImgPrefabの設定用
-    public GameObject ItemPrefab;
+	public GameObject ItemPrefab;
     public GameObject ItemPanel;
+
 
     //UIの描画
 	public void SetUI()
-    {
+	{
         //一回、itempanelの子要素を全削除
         //※全削除→再描画方式が最善かは要検討。順番やエフェクト等考えるとなんだかんだ一番効率的な気はしている。
         for (int j = ItemPanel.transform.childCount - 1; j >= 0; j--)
@@ -23,18 +26,18 @@ public class GameDirector : MonoBehaviour
             GameObject.DestroyImmediate(ItemPanel.transform.GetChild(j).gameObject);
         }
 
+        //リストにしたがってアイテムを順に描画。
 		var flowerList = SceneManager.instance.fList;
 
-        //リストにしたがってアイテムを順に描画。
 		for (int i = flowerList.Count - 1; i > -1 ; i--)
         {
-            GameObject item = Instantiate(ItemPrefab) as GameObject;
 			var fItem = flowerList[i];
+            GameObject item = Instantiate(ItemPrefab) as GameObject;
 			item.name = fItem.name;
             item.transform.SetParent(ItemPanel.transform, false);
             
+			// 画像
             Image img = item.GetComponent<Image>();
-			//img.sprite = Resources.Load<Sprite>(fItem.name);
 			var sprite = Resources.Load<Sprite>(fItem.name);
 			if( sprite == null )
 			{
@@ -42,40 +45,67 @@ public class GameDirector : MonoBehaviour
 				sprite = Resources.Load<Sprite>("cat");
 			}
 			img.sprite = sprite;
-
+			// 所持数
             Text txt = item.transform.FindChild("Text").gameObject.GetComponent<Text>();
 			txt.text = "" + fItem.count;
 
             //アニメーションを追加
             Animator animator = item.GetComponent<Animator>();
+			if( animator == null )
+			{
+				Debug.LogWarning( item.name + "にAnimatorがセットされてない" );
+				return;
+			}
 			animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(fItem.name);
-
         }
     }
+	/*
+    //持ち物の加算
+    public void CountObj(string flowername)
+    {
 
-    // Use this for initialization
-    void Start () 
-	{
-		instance = this;
+        int flag = name_List.IndexOf(flowername);
+ 
+        //既に持ち物にある場合は個数を1追加
+        if(flag >= 0)
+        {
+            num_List[flag]++;
+        }
+        //ない場合は、新たなnameとして追加して、個数は1に。
+        else
+        {
+            name_List.Add(flowername);
+            num_List.Add(1);
+        }
 
-        // Listに初期値を設定
-		/*
-		var item = new SceneManager.FlowerItem();
-		item.name = "tama";
-		item.count = 2;
-		SceneManager.instance.fList.Add( item );
-
-		item = new SceneManager.FlowerItem();
-		item.name = "yama";
-		item.count = 3;
-		SceneManager.instance.fList.Add( item );
-
-		item = new SceneManager.FlowerItem();
-		item.name = "blcok";
-		item.count = 1;
-		SceneManager.instance.fList.Add( item );
-		*/
         //UIを描画
         SetUI();
     }
+	*/
+
+
+    // Use this for initialization
+    void Start () {
+
+        // Listに初期値を設定
+//       name_List.Add("maru");
+ //       num_List.Add(2);
+  //      name_List.Add("sankaku");
+   //     num_List.Add(3);
+    //    name_List.Add("shikaku");
+     //   num_List.Add(1);
+
+        //UIを描画
+        SetUI();
+
+		instance = this;
+    }
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
